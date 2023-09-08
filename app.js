efc6a126de7e2dddbc95f5b59a8d4eb5a6e968e5
amf1ra <josephcres1407@gmail.com>
2023-09-08 11:44:32 -0400

    ola

---
+++ app.js
@@ -0,0 +1,73 @@
+(function(){
+    const listElements = document.querySelectorAll('.menu__item--show');
+    const list = document.querySelector('.menu__links');
+    const menu = document.querySelector('.menu__hamburguer');
+
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click', ()=>{
+
+                
+                let subMenu = element.children[1];
+                let height = 0;
+                element.classList.toggle('menu__item--active');
+
+
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+
+                subMenu.style.height = `${height}px`;
+
+            });
+        });
+    }
+
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element=>{
+
+            if(element.children[1].getAttribute('style')){
+                element.children[1].removeAttribute('style');
+                element.classList.remove('menu__item--active');
+            }
+
+        });
+    }
+
+
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth > 800){
+            deleteStyleHeight();
+            if(list.classList.contains('menu__links--show'))
+                list.classList.remove('menu__links--show');
+
+        }else{
+            addClick();
+        }
+    });
+
+    if(window.innerWidth <= 800){
+        addClick();
+    }
+
+    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
+
+
+
+})();
+
+
+const botonDesplegar = document.getElementById('botonDesplegar');
+const contenido = document.getElementById('contenido');
+
+botonDesplegar.addEventListener('click', () => {
+    if (contenido.style.display === 'none' || contenido.style.display === '') {
+        contenido.style.display = 'block';
+        botonDesplegar.textContent = 'Show less';
+    } else {
+        contenido.style.display = 'none';
+        botonDesplegar.textContent = 'See more details';
+    }
+});
+
+
